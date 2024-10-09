9b0be1693a3f4a0c0da4dc3002939bbd8a0417c7
Quang Hưng <110990347+hung20gg@users.noreply.github.com>
2024-10-09 21:04:24 +0700

    Hung20gg (#14)
    * not yet done but just push for fun
    
    * code for branch llm
    
    * lots of bug
    
    * .

--- sample/branch_reasoning.py
+++ sample/branch_reasoning.py
@@ -1,5 +1,5 @@
 from llm.llm_utils import get_code_from_text_response, get_json_from_text_response
-from .setup_db import DBHUB
+from setup_db import DBHUB
 import utils
 import numpy as np
 import pandas as pd
@@ -42,9 +42,20 @@ def company_name_to_stock_code(db : DBHUB, names, method = 'similarity') -> pd.D
             result = pd.DataFrame()
         return result
         
+def get_company_detail_from_df(df, db: DBHUB) -> pd.DataFrame:
+    stock_code = set()
+    
+    for col in df.columns:
+        if col == 'stock_code':
+            stock_code.update(df[col].tolist())
+        if col == 'company_name':
+            stock_code.update(company_name_to_stock_code(db, df[col].tolist())['stock_code'].tolist())
+        if col == 'invest_on':
+            stock_code.update(company_name_to_stock_code(db, df[col].tolist())['stock_code'].tolist())
+    
+    # Get the detail of the company (Not done yet)
 
-
-def simplify_branch_reasoning(llm, task, num_steps=3):
+def simplify_branch_reasoning(llm, task, num_steps=2, verbose=False):
     """
     Breaks down the task into simpler steps
     """
@@ -52,7 +63,7 @@ def simplify_branch_reasoning(llm, task, num_steps=3):
     messages = [
         {
             "role": "system",
-            "content": f"You are an expert in financial statement and database management. You are tasked to break down the given task to {num_steps-1}-{num_steps+1} simpler SQL query steps. Please provide the steps."
+            "content": f"You are an expert in financial statement and database management. You are tasked to break down the given task to {num_steps-1}-{num_steps+1} simpler steps. Please provide the steps."
         },
         {
             "role": "user",
@@ -63,22 +74,43 @@ Question: {task}
 </question>
 Here are some information you might need:        
 {utils.read_file_without_comments("prompt/breakdown_note.txt")}   
-Thinking and return the steps in JSON format.
+
+<example>
+### Task: Calculate ROA, ROE of all the company which are owned by VinGroup
+
+Step 1: Find the stock code of the company that is owned by VinGroup in `category_code` database.
+
+Step 2: Calculate ROA, ROE of the choosen stock codes in the `financial_statement` database.
+
+</example>
+
+Note:
+ - You should provide general steps to solve the question. 
+ - You must not provide the SQL query. 
+ - Each step should be as independence as possible.
+ - The number of steps should be lowest if possible.
+ - You don't need to provide the steps that are too obvious (retrieve).
+ 
+Based on the question and databse, thinking and return the steps in JSON format.
     ```json
-    {
+    {{
         "steps" : ["Step 1", "Step 2"]
-    }
+    }}
     ```         
 """
         }  
     ]
     
     response = llm(messages)
-    return get_json_from_text_response(response, new_method=True)
+    if verbose:
+        print("Branch reasoning response: ")
+        print(response)
+        print("====================================")
+    return get_json_from_text_response(response, new_method=True)['steps']
 
 
 
-def get_stock_code_based_on_company_name(llm, task):
+def get_stock_code_based_on_company_name(llm, task, verbose=False):
     """
     Get the stock code based on the company name
     """
@@ -101,23 +133,28 @@ Return an empty list if no company name is found.
 """}]
     
     response = llm(messages)
-    return get_json_from_text_response(response, new_method=True)
+    if verbose:
+        print("Get stock code based on company name response: ")
+        print(response)
+        print("====================================")
+    return get_json_from_text_response(response, new_method=True)['company_name']
 
 
 
-def llm_branch_reasoning(llm, task, db: DBHUB):
+def llm_branch_reasoning(llm, task, db: DBHUB, verbose=False):
+
     """
     Branch reasoning for financial statement
     """
     
-    steps = simplify_branch_reasoning(llm, task)['steps']
+    steps = simplify_branch_reasoning(llm, task, verbose=verbose)
     steps_string = ""
     
     for i, step in enumerate(steps):
         steps_string += f"Step {i+1}: \n {step}\n\n"
     
     # Check step 1: Extract company name
-    checkpoints_message = "Find the stock code of the listed company name"
+    checkpoints_message = "Find the stock code of the company name in the given question"
     look_up_stock_code = ""
     
     history = [
@@ -127,8 +164,7 @@ def llm_branch_reasoning(llm, task, db: DBHUB):
         },
         {
             "role": "user",
-            "content": f"""
-You have the following database schema:
+            "content": f"""You have the following database schema:
 <description>
 {utils.read_file_without_comments('prompt/seek_database.txt')}
 </description>
@@ -138,7 +174,12 @@ Here is a natural language query that you need to convert into a SQL query:
 {task}
 </query>    
 
-{find_suitable_column(llm, task, db=db)}  
+   
+Note:
+- Your SQL query must only access the database schema provided.
+- In each step, you should only do the task that is required. Do not do the task of next step.
+- Make the SQL query as simple and readable as possible. Try to use the existing tables and columns from previous query.
+- If the data provided is enough to answer the question, you don't need to do return the query.
         
 Here are the steps to break down the task:
 <steps>
@@ -148,33 +189,34 @@ Here are the steps to break down the task:
         }
     ]
     
-    cur_step = 1
+    cur_step = 0
     # Get company stock code
-    if utils.edit_distance_score(steps[0].lower(), checkpoints_message.lower()) > 0.6:
-        print("Step 1: Extract company name")
-        company_names = get_stock_code_based_on_company_name(llm, task)
-        stock_code_table = utils.df_to_markdown(company_name_to_stock_code(db, company_names))
-        look_up_stock_code = f"For first step, here are the stock codes of the companies: \n{stock_code_table}"
-        history[-1]["content"] += look_up_stock_code
-        
-        cur_step += 1
-        steps = steps[1:]
+    # Need to make a copy to add new company table everytimes the code find a new company
+
+    print("Step 0: Extract company name")
+    company_names = get_stock_code_based_on_company_name(llm, task, verbose=verbose)
+    stock_code_table = utils.df_to_markdown(company_name_to_stock_code(db, company_names))
+    look_up_stock_code = f"\nHere are the stock codes of the companies: \n\n{stock_code_table}"
+    history[-1]["content"] += look_up_stock_code
+
         
     # Other steps
     for i, step in enumerate(steps):
         cur_step += 1
         print(f"Step {cur_step}: {step}")
         
-        if i == 0:
-            history[-1]["content"] += f"\nDo the Step {cur_step}: {step}"
-        else:
-            history.append({
-                "role": "user",
-                "content": f"Do the Step {cur_step}: {step}"
-            })
+        history.append({
+            "role": "user",
+            "content": f"Do the Step {cur_step}: {step}\n\nHere is the sample SQL you might need\n\n{db.find_sql_query(step)}"
+        })
+        
+        print("RAG for step: ", cur_step, db.find_sql_query(step))
         
         response = llm(history)
-        
+        if verbose:
+            print(f"Step {cur_step} response: ")
+            print(response)
+            print("====================================")
         # Check TIR 
         codes = get_code_from_text_response(response)
         
@@ -187,7 +229,9 @@ Here are the steps to break down the task:
                 table_markdown = utils.df_to_markdown(table)
                 TIR_response += f"SQL result for {j+1}: \n{table_markdown}\n\n"
                 
-        response += "\n\n" + TIR_response
+        # Add fix bug if code is not runnable
+                
+        response += "\n\n### The result of the given SQL:\n\n" + TIR_response
         
         history.append({
             "role": "assistant",
@@ -197,15 +241,15 @@ Here are the steps to break down the task:
     return history
         
     
-def find_suitable_column(llm, text, return_type='markdown', db: DBHUB = None, top_k=5):
-    
+def find_suitable_column(llm, text, steps='', return_type='markdown', db: DBHUB = None, top_k = 5, verbose=False):
+
     system_prompt = """
     You are an expert in analyzing financial reports. 
     """
     
     prompt = f"""
     {text}
-    
+    {steps}
     <task>
     Based on given question, analyze and suggest the suitable column in the financial statement that can be used to answer the question.
     Notice that there are two types of financial statements: one for banks and one for non-banks cooperate.
@@ -236,6 +280,10 @@ def find_suitable_column(llm, text, return_type='markdown', db: DBHUB = None, to
     ]
     
     response = llm(messages)
+    if verbose:
+        print("Find suitable column response: ")
+        print(response)
+        print("====================================")
     
     extracted_column = get_json_from_text_response(response, new_method=True)
     
--- sample/utils.py
+++ sample/utils.py
@@ -2,7 +2,7 @@ def read_file_without_comments(file_path):
     with open(file_path, 'r') as f:
         lines = f.readlines()
         lines = [line for line in lines if not line.startswith('#') and not line.startswith('//')]
-        return ''.join(lines)
+        return '\n'.join(lines)
     
 def read_file(file_path):
     with open(file_path, 'r') as f:
